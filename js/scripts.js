36ecbebecd72579233c3e1db8e693b3ad92dcaed
Sebastian Rojek <seriu54@gmail.com>
2016-10-25 22:15:20 +0200

    "triangle Area"

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+// Pole trójkąta
+
+var triangle = {
+  a: 5,
+  h: 2,
+  triangleArea: function() {
+    return this.a * this.h / 2;
+  }
+};
+
+triangle.a = prompt("Enter width");
+triangle.h = prompt("Enter height");
+console.log('Pole trójkąta o podstawie a: ' + triangle.a + ' i wysokości h: ' + triangle.h + ' wynosi: ' + triangle.triangleArea());
+alert('Pole trójkąta o podstawie a: ' + triangle.a + ' i wysokości h: ' + triangle.h + ' wynosi: ' + triangle.triangleArea());
